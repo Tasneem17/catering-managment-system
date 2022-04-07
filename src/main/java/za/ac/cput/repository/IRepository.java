583dc26b166016e566f4d8d4d0330421a40a0de0
Wilbur Smith <220003033@mycput.ac.za>
2022-04-07 15:31:16 +0200

    Setting up the project

---
+++ src/main/java/za/ac/cput/repository/IRepository.java
@@ -0,0 +1,9 @@
+/*  IRepository.java
+    Generic repository of the system
+    Author: Wilbur Deano Smith (220003033)
+    Date: 7 April 2022
+*/
+package za.ac.cput.repository;
+
+public interface IRepository {
+}
