d7c4cc4c1b6b8c8f3a6e2c6eed2642c3441952ec
Wilbur Smith <220003033@mycput.ac.za>
2022-04-10 08:50:19 +0200

    Creating User Repository

--- src/main/java/za/ac/cput/repository/UserRepository.java
+++ src/main/java/za/ac/cput/repository/UserRepository.java
@@ -1,11 +1,15 @@
+/*  UserRepository.java
+    Class to manage users
+    Author: Wilbur Deano Smith (220003033)
+    Date:  10 April 2022
+*/
 package za.ac.cput.repository;
 
 import za.ac.cput.entity.User;
-
 import java.util.HashSet;
 import java.util.Set;
 
-public class UserRepository
+public class UserRepository implements IUser
 {
     private static UserRepository repository = null;
     private Set<User> userDB = null;
@@ -24,13 +28,52 @@ public class UserRepository
         return repository;
     }
 
-    public void create(){}
+    @Override
+    public User create(User user)
+    {
+        boolean success = userDB.add(user);
+        if (!success)
+            return null;
+        return user;
+    }
 
-    public void read(){}
+    @Override
+    public User read(String userID)
+    {
+        for (User user :userDB)
+        {
+            if (user.getUserID().equals(userID))
+                return user;
+        }
+        return null;
+    }
 
-    public void update(){}
+    @Override
+    public User update(User user)
+    {
+        User oldUser = read(user.getUserID());
+        if(user != null)
+        {
+            userDB.remove(oldUser);
+            userDB.add(user);
+            return user;
+        }
+        return null;
+    }
 
-    public void delete(){}
+    @Override
+    public boolean delete(String userID)
+    {
+        User userToDelete = read (userID);
+        if (userToDelete == null)
+            return false;
+        userDB.remove(userToDelete);
+        return true;
+    }
 
-    public void getAll(){}
+    @Override
+    public Set<User> getAll()
+    {
+        return  userDB;
+    }
 }
