53ade6f12e044a91d7d279670e14029cc352a603
Wilbur Smith <220003033@mycput.ac.za>
2022-08-11 19:42:56 +0200

    Setup for service

---
+++ src/main/java/za/ac/cput/service/impl/UserRoleServiceimpl.java
@@ -0,0 +1,36 @@
+package za.ac.cput.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import za.ac.cput.entity.UserRole;
+import za.ac.cput.repository.UserRoleRepository;
+import za.ac.cput.service.UserRoleService;
+
+import java.util.Optional;
+
+@Service
+public class UserRoleServiceimpl implements UserRoleService {
+
+    private UserRoleRepository userRoleRepository;
+
+    @Autowired
+    public UserRoleServiceimpl(UserRoleRepository userRoleRepository)
+    {
+        this.userRoleRepository = userRoleRepository;
+    }
+
+    @Override
+    public UserRole save(UserRole userRole) {
+        return this.userRoleRepository.save(userRole);
+    }
+
+    @Override
+    public Optional<UserRole> read(UserRole.UserRoleId userRoleId) {
+        return this.userRoleRepository.findById(userRoleId);
+    }
+
+    @Override
+    public void delete(UserRole userRole) {
+        this.userRoleRepository.delete(userRole);
+    }
+}
